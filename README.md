b792942439d389efca804640b23d520ba5f68189
kurniaeliazar <kurniaeliazar@gmail.com>
2021-09-20 12:43:32 +0700

    second changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PemrogramanObyekSatu
 Git repo untuk pemrograman obyek 1
+
+## Description
+This is an example repo for sending the tasks
--- Tugas1/HelloWorld.java
+++ Tugas1/HelloWorld.java
@@ -1,5 +1,6 @@
 public HelloWorld{
     public static void main(String args []){
         System.out.println("Hallo");
+        System.out.println("Hallo Lagi");
     }
 }
